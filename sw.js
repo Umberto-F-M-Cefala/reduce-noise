bd344ceedae92a399b49d63c9f8216e07172a042
Umberto F. M. Cefalà <umberto.cefala@outlook.com>
2026-08-27 19:47:18 +0200

    Add service worker for caching resources

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'reduce-noise';
+
+const urlsToCache = [
+  '/reduce-noise/',
+  '/reduce-noise/index.html',
+  '/reduce-noise/icon-192x192.png',
+  '/reduce-noise/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting(); // usa subito la nuova versione
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim(); // aggiorna subito tutte le finestre aperte
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
